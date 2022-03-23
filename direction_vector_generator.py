3e5dff1c8851876e3d1a9bdf9673bb3792ff3d5c
davidcGIThub <davidc@linuxpopper.org>
2022-03-23 17:53:14 -0600

    direction generator

--- direction_vector_generator.py
+++ direction_vector_generator.py
@@ -8,29 +8,28 @@ import numpy as np
 from scipy.optimize import fsolve
 
 class DirectionVectorGenerator:
-    def __init__(self , resolution,
-                        image_size=(640,480)):
+    def __init__(self , resolution, image_size=(640,480)):
+        self._image_width = image_size[0]
+        self._image_height = image_size[1]
+        self._origin = (int(self._image_width/2),self._image_height)
+        self._masks = []
+        self._angles = []
+        self.generate_masks_and_angles()
 
-
-    width = image_size[0]
-    height = image_size[1]
-    origin = (int(width/2),height)
-
-    # Generate masks and their angles
-    xs = np.linspace(0,width,11)
-    blank = np.zeros((height, width))
-    self.masks = []
-    self.angles = []
-    for x in xs:
-        endpoint = (int(x), 0)
-        mask = np.copy(blank)
-        cv2.line(mask, origin, endpoint, 255, 1)
-        cv2.imshow("mask", mask)
-        cv2.waitKey(0)
-        self.masks.append(mask)
-        if x != width/2:
-            angle = np.arctan((x-width/2)/height)
-        else:
-            angle = 0
-        self.angles.append(angle)
+    def generate_masks_and_angles(self):
+        # Generate masks and their angles
+        xs = np.linspace(0,self._image_width,11)
+        blank = np.zeros((self._image_height, self._image_width))
+        for x in xs:
+            endpoint = (int(x), 0)
+            mask = np.copy(blank)
+            cv2.line(mask, self._origin, endpoint, 255, 1)
+            cv2.imshow("mask", mask)
+            cv2.waitKey(0)
+            self._masks.append(mask)
+            if x != self._image_width/2:
+                angle = np.arctan((x-self._image_width/2)/self._image_height)
+            else:
+                angle = 0
+            self._angles.append(angle)
         
