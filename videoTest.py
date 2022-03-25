f4da50c9dc6a6b15bf46824d6ea73f7a90bda119
Alex Jordan <a.jordan6955@gmail.com>
2022-03-25 11:56:32 -0600

    vid test

---
+++ videoTest.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+from direction_vector_generator import DirectionVectorGenerator
+from reactive_controller import ReactiveController
+from birdseye import birdsEye
+import time
+
+file = r"C:\Downloads\\Video.avi"
+file = "Video.avi"
+video = cv2.VideoCapture(file)
+processor = birdsEye(file = "./visionracer/transform2.npz")
+
+intersects = DirectionVectorGenerator(11, (640, processor.height))
+controller = ReactiveController(velocity_gain=2.5/640, angle_gain=1)
+img_file = "./pictures/"
+while(video.isOpened()):
+    ret, img = video.read()
+    if ret:
+        cv2.imshow("test", img)
+        processor.process(img)
+        max_stream_length, stream_angle, mask = intersects.get_direction_vector(processor.combined)
+        mask = cv2.resize(mask, (640,316))
+        cv2.imshow("obstacles", processor.combined+mask)
+        # Control
+        velocity_command, angle_command = controller.proportional_control(max_stream_length, stream_angle) 
+        print(velocity_command, angle_command)
+
+        k= cv2.waitKey(1)
+        if k == ord("q"):
+            break
+
+    else:
+        cv2.destroyAllWindows()
+        break
