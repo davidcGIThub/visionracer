e100f31bad1756bd01828168df7c3b2b733d6888
Alex Jordan <a.jordan6955@gmail.com>
2022-03-26 17:08:14 -0600

    save

--- ECEnRacer_average.py
+++ ECEnRacer_average.py
@@ -54,7 +54,7 @@ Car.pid(1)          # Use PID control
 num_encoder_same = 0
 
 # Wait for user input to continue
-input("Visionracer Initialized\n Press any key to start your race!")
+wait = input("Visionracer Initialized\n Press any key to start your race!")
 
 while True:
     (t, rgb, depth, accel, gyro) = rs.getData()
